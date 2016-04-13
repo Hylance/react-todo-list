8292d4ad0fc5959961f50bc864020090fdd6fab4
shuheng guo <shuheng.guo@orbitz.com>
2016-04-12 23:14:55 -0500

    part 3

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import {render} from 'react-dom' // var render = require('react-dom').render
+import App from '../components/App'
 
+render(
+	// define the encompassing component
+	// Dom element we want to mount it to
+	<App/>,
+	document.getElementById('app')
+
+)
