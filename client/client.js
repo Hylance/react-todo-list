5d712b24cbc355b1d32ac03f27f9c43f1ceec7c3
shuheng guo <shuheng.guo@orbitz.com>
2016-04-18 10:50:17 -0500

    redux

--- client/client.js
+++ client/client.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {render} from 'react-dom' // var render = require('react-dom').render
 import App from '../components/App'
 
+//configure and create our store
+//var store = createStore(reducer, initialState)
+
 render(
 	// define the encompassing component
 	// Dom element we want to mount it to
--- components/TodoInput.js
+++ components/TodoInput.js
@@ -15,22 +15,17 @@ class TodoInput extends Component {
 		})
 	}
 
-	deleteLetter() {
-		this.setState({
-			inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
-		});
-	}
 
 	render() {
 		return (
 			<div>
 				<input 
 				type="text"
-				placeholder="This is going to be text"
+				placeholder="Type in your to do"
 				value={this.state.inputText}
 				onChange={this.handleChange.bind(this)}
 				/>
-				<TextDisplay text={this.state.inputText} deleteLetter={this.deleteLetter.bind(this)}/>
+				<button> Submit </button>
 			</div>
 		)
 			
